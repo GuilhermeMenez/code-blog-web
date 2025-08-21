bae47963ce31cacae240b653024ddf0c3109ea9f
GuilhermeMenez <guilhermetav@edu.unirio.br>
2025-08-20 23:46:20 -0300

    ciro me aiudou

--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -4,24 +4,24 @@ import { AuthContextType, AuthProviderProps, User } from "../types/authTypes";
 export const AuthContext = createContext<AuthContextType>({} as AuthContextType);
 
 const AuthProvider = ({ children }: AuthProviderProps) => {
-    const [user, setUser] = useState<User | null>(null);
+    const [user, setUser] = useState<User>({});
     const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
     const [validateAuth, setValidateAuth] = useState<boolean>(false);
 
     return (
         <AuthContext.Provider
-            value= {{
-        user,
-            setUser,
-            isAuthenticated,
-            setIsAuthenticated,
-            validateAuth,
-            setValidateAuth,
+            value={{
+                user,
+                setUser,
+                isAuthenticated,
+                setIsAuthenticated,
+                validateAuth,
+                setValidateAuth,
             }
-}
+            }
         >
-    { children }
-    </AuthContext.Provider>
+            {children}
+        </AuthContext.Provider>
     );
 };
 
